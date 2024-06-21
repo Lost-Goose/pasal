4920888f044ac5df62af65482cd4fc98fb270b9b
Lost-Goose <ashim431@outlook.com>
2024-06-21 11:32:59 +1000

    Add files via upload

---
+++ pasal.js
@@ -0,0 +1,17 @@
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   const searchIcon = document.getElementById("search-icon");
+//   const searchBar = document.getElementById("search-bar");
+
+//   searchIcon.addEventListener("click", function () {
+//     searchBar.classList.toggle("show");
+//   });
+
+//     // Close the search bar when clicking outside of it
+//   document.addEventListener("click", function (event) {
+//     if (!searchBar.contains(event.target) && !searchIcon.contains(event.target)) {
+//       searchBar.classList.remove("show");
+//     }
+//   });
+// });
+
